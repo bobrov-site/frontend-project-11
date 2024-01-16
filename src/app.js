560421f0421e67f0834beda9732f34548e9309c9
Daniil Bobrov <allodrmen@yandex.ru>
2024-01-16 16:15:54 +0300

    created app.js in src folder

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import onChange from "on-change";
+import axios from 'axios';
+
+const state = {
+    elements: {
+      input: document.getElementById('url-input')
+},
+    feeds: [],
+    posts: [],
+    url: '',
+}
+
+const validateForm = () => {
+
+}
+export default (() => {
+    console.log('hello');
+})
