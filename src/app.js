8369ef3ad400dcc81c4ae0591973019c791f3d2c
Daniil Bobrov <allodrmen@yandex.ru>
2024-01-17 10:32:58 +0300

    create generateSchema, import yup, view, onChange. Add submit event

--- src/app.js
+++ src/app.js
@@ -1,18 +1,42 @@
-import onChange from "on-change";
+import onChange from 'on-change';
 import axios from 'axios';
+import * as yup from 'yup';
+import view from './view.js';
 
 const state = {
-    elements: {
-      input: document.getElementById('url-input')
-},
-    feeds: [],
-    posts: [],
-    url: '',
-}
+  form: {
+    process: 'filling',
+    error: '',
+  },
+  elements: {
+    form: document.querySelector('.rss-form'),
+    input: document.getElementById('url-input'),
+  },
+  feeds: [],
+  posts: [],
+  url: '',
+};
 
-const validateForm = () => {
+const generateSchema = () => {
+  const urlsList = state.feeds.map((feed) => feed.url);
+  return yup.object({
+    url: yup.string().url('Неверная ссылка').required('Обязательное поле').notOneOf(urlsList, 'RSS уже существует'),
+  });
+};
 
-}
+const watchedState = onChange(state, view(state));
 export default (() => {
-    console.log('hello');
-})
+  state.elements.form.addEventListener('submit', ((event) => {
+    event.preventDefault();
+    const url = new FormData(event.target).get('url');
+    generateSchema().validate({ url }).then((response) => {
+      console.log(response, 'res');
+    })
+      .catch((e) => {
+        watchedState.form.error = e.message;
+      });
+  }));
+});
+
+// https://lorem-rss.hexlet.app/feed
+// https://ru.hexlet.io/lessons.rss
