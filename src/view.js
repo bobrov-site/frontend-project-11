3d5e4ebf23123102b90772153b634a5b5bb97141
Daniil Bobrov <allodrmen@yandex.ru>
2024-01-17 11:22:20 +0300

    add renderError in view.js

--- src/view.js
+++ src/view.js
@@ -1,4 +1,16 @@
+const renderError = (state, message) => {
+    if (message) {
+        state.elements.feedback.innerHTML = message;
+        state.elements.input.classList.add('is-invalid');
+    }
+    else {
+        state.elements.feedback.innerHTML = '';
+        state.elements.input.classList.remove('is-invalid');
+    }
+}
+
 export default (state) => (path, value) => {
-  console.log(state, 'view');
-  console.log(path, value);
+    if (path === 'form.error') {
+        renderError(state, value);
+    }
 };
