2b07cdbf1324ea5546d7af5c2788aa1813c8a21e
Daniil Bobrov <allodrmen@yandex.ru>
2024-01-17 15:59:42 +0300

    created index.js in locales

---
+++ src/locales/index.js
@@ -0,0 +1,9 @@
+const resourses = {
+    translation: {
+        error_wrong_link: 'Неверная ссылка',
+        error_required: 'Это поле обязательно для заполнения',
+        error_not_unique: 'RSS уже существует',
+    }
+}
+
+export default resourses;
