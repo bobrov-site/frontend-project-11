fa5198c5b566a4e5f16ecf334d251e453e49134f
Daniil Bobrov <allodrmen@yandex.ru>
2024-01-22 11:50:07 +0300

    render column feeds in view.js

--- src/view.js
+++ src/view.js
@@ -33,10 +33,29 @@ const createContentCard = (title) => {
 };
 
 const renderColumnFeed = (state, i18nextInstance) => {
-  // TODO доделать карточки
-  const { elements } = state;
-  const card = createContentCard(i18nextInstance.t('feedsTitle'));
-  elements.feedsColumn.append(card);
+  const { elements, feeds } = state;
+  if (!elements.feedsColumn.hasChildNodes()) {
+    const card = createContentCard(i18nextInstance.t('feedsTitle'));
+    elements.feedsColumn.append(card);
+  }
+  const card = elements.feedsColumn.querySelector('.card');
+  const list = card.querySelector('ul');
+  //TODO вопрос по оптимазици, можно ли так делать, и как лучше сделать, чтобы добавлять по одной единицы элементов, вместо
+  // нескольких?
+  list.innerHTML = '';
+  const items = feeds.map((feed) => {
+    const item = document.createElement('li');
+    const title = document.createElement('h3');
+    const description = document.createElement('p');
+    description.classList.add('m-0', 'small', 'text-black-50');
+    item.classList.add('list-group-item', 'border-0', 'border-end-0');
+    title.classList.add('h6', 'm-0');
+    title.innerHTML = feed.title;
+    description.innerHTML = feed.description;
+    item.append(title, description);
+    return item;
+  });
+  list.append(...items);
 };
 
 const disableSendButton = (state) => {
