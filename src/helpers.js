106d3fed8527fd626a43efa78f557047326011e7
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-12 22:25:33 +0300

    remove helpers and added helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,8 @@
-export default (originalUrl) => {
+const buildUrl = (originalUrl) => {
     const url = new URL('https://allorigins.hexlet.app/get');
     url.searchParams.set('disableCache', true);
     url.searchParams.set('url', originalUrl);
     return url.href;
 };
+
+export default buildUrl;
